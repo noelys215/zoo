021acb170ae77f46734c14081beb93dc51e176a5
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-01-30 18:35:36 -0500

    first commit

---
+++ Zookeeper with Java/task/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("I love animals!");
+        System.out.println("Let's check on the animals...");
+        System.out.println("The deer looks fine.");
+        System.out.println("The bat looks happy.");
+        System.out.println("The lion looks healthy.");
+    }
+}
