4532d399fd1ea68e6da2f5976b3510547e835c5e
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-02-04 22:00:57 -0500

    added second challenge

--- Zookeeper with Java/task/src/Main.java
+++ Zookeeper with Java/task/src/Main.java
@@ -1,12 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("I love animals!");
-        System.out.println("Let's check on the animals...");
-        System.out.println("The deer looks fine.");
-        System.out.println("The bat looks happy.");
-        System.out.println("The lion looks healthy.");
+        String camel = """
+                Switching on the camera in the camel habitat...
+                 ___.-''''-.
+                /___  @    |
+                ',,,,.     |         _.'''''''._
+                     '     |        /           \\
+                     |     \\    _.-'             \\
+                     |      '.-'                  '-.
+                     |                               ',
+                     |                                '',
+                      ',,-,                           ':;
+                           ',,| ;,,                 ,' ;;
+                              ! ; !'',,,',',,,,'!  ;   ;:
+                             : ;  ! !       ! ! ;  ;   :;
+                             ; ;   ! !      ! !  ; ;   ;,
+                            ; ;    ! !     ! !   ; ;
+                            ; ;    ! !    ! !     ; ;
+                           ;,,      !,!   !,!     ;,;
+                           /_I      L_I   L_I     /_I
+                Look at that! Our little camel is sunbathing!""";
 
-        long val = 120L;
-        System.out.printf("The distance between two towns %d", val);
+        System.out.println(camel);
     }
 }
